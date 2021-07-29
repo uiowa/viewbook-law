eadf3d46d16f6c50158603c75d551eba63d5767b
makurj <makur-jain@uiowa.edu>
2021-07-29 10:51:19 -0500

    requested changes

--- src/App.vue
+++ src/App.vue
@@ -156,6 +156,20 @@ personal approach to legal education makes it easy to build strong connections w
   <div class="v-dots-scroll-target" id="hawkeyes"></div>
 </div>
 
+  <banner
+    image=""
+    banner_size="small"
+    banner_vertical_alignment="vertical-center"
+    banner_horizontal_alignment="horizontal-center"
+    banner_classes="bg--gold--pattern--brain brain banner--slim-title"
+    banner_type=""
+    banner_overlay= ""
+    banner_text=''
+    banner_title='See what students are saying about Iowa Law'
+    banner_title_classes='headline headline--uppercase'
+  />
+
+
 <div class="grid--threecol--33-34-33">
   <div class="list-container">
     <Card
@@ -165,7 +179,7 @@ personal approach to legal education makes it easy to build strong connections w
       content="
         'I wanted to see health law from the client’s perspective. Getting to interact with patients and medical providers for a week was absolutely invaluable. I would honestly say this experience was the single-most practical learning experience I’ve had throughout my legal education.'
       "
-      class="card--stacked bg--black card--centered-left card--alignment-left"
+      class="card--stacked card--centered-left card--alignment-left card--padded"
       media_class="card__media--large card__media card__media"
       img_class=""
       headline_class="h3 headline headline--uppercase"
@@ -180,7 +194,7 @@ personal approach to legal education makes it easy to build strong connections w
       content="
         'My goal is to help the most vulnerable people in our criminal legal system, and the best way to do that is to provide an outstanding legal defense for those who have been wrongfully accused and treated unfairly, to stand up in court and tell their story.'
       "
-      class="card--stacked bg--black card--centered-left card--alignment-left"
+      class="card--stacked card--centered-left card--alignment-left card--padded"
       media_class="card__media--large card__media card__media"
       img_class=""
       headline_class="h3 headline headline--uppercase"
@@ -195,7 +209,7 @@ personal approach to legal education makes it easy to build strong connections w
       content="
         'While writing my brief and preparing for oral arguments, I realized that Iowa Law had equipped me with the necessary skills to excel. Participating in the Appellate Advocacy program made me more confident, prepared, and equipped to take on the legal world and my legal career.'
       "
-      class="card--stacked bg--black card--centered-left card--alignment-left"
+      class="card--stacked card--centered-left card--alignment-left card--padded"
       media_class="card__media--large card__media card__media"
       img_class=""
       headline_class="h3 headline headline--uppercase"
@@ -419,6 +433,12 @@ $imgpath: '../node_modules/@uiowa/uids/src/assets/images';
   }
 }
 
+.grid-panel__column-content-right.grid-panel__content, .grid-panel__column-content-left.grid-panel__content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 3rem 3.5rem 3rem 3rem;
+}
 
 .element--circle-list ul {
   margin-top: 1.05rem;
@@ -426,11 +446,17 @@ $imgpath: '../node_modules/@uiowa/uids/src/assets/images';
   margin-left: 1.875rem;
 }
 
-.card p {
-  margin-top: 0.625rem;
+.card {
+  p {
+    margin-top: 0.625rem;
+  }
+
+  &--padded .card__body {
+    padding: 2rem;
+  }
 }
 
-// Override and condense for smaller devices.
+  // Override and condense for smaller devices.
 .menu-group__wrapper {
   transition: padding 0.5s ease;
   padding: 0.3rem 0;
